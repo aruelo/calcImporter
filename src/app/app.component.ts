8d862bf41a78785a23c76ca0b8a98c9bebb16c9d
Ariel Gonzalez <areuel@hotmail.com>
2020-06-15 15:49:06 -0300

    Ya mapeado el form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc-importer';
+  _form: FormGroup;
+
+	constructor(
+		private fb: FormBuilder,
+
+	) {
+		this._form =  this.fb.group({
+			Categoria : [null, Validators.required],
+			Precio : [null, Validators.required],
+			Peso: [null, Validators.required],
+			Unidad_Peso: [null, Validators.required],
+			Largo: [null, Validators.required],
+			Ancho: [null, Validators.required],
+			Profundidad: [null, Validators.required],
+			Unidad_Medidas: [null, Validators.required],
+		});
+	}
+	protected _doCalc () {
+		console.log("PITO");
+		console.log(this._form.value);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule, MatButtonModule, MatGridListModule, MatSelectModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
 	BrowserAnimationsModule,
 	FormsModule,
+	ReactiveFormsModule,
 	MatCardModule,
 	MatButtonModule,
 	MatGridListModule,
